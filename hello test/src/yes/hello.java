5bba79097d68095f0c757b333e4b97d7f88e2ed0
nathanaelmoyer <36149055+nathanaelmoyer@users.noreply.github.com>
2019-09-11 16:50:40 -0400

    Initial commit
    Trying to learn Java

---
+++ hello test/src/yes/hello.java
@@ -0,0 +1,16 @@
+package yes;
+
+public class hello {
+	public hello() {
+    }
+    
+	String word = "I hope this works";
+	
+    public void largest()
+    {
+    	System.out.print(word);
+    	
+    }
+
+}
+
---
+++ hello test/src/yes/testHello.java
@@ -0,0 +1,14 @@
+package yes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testHello {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
